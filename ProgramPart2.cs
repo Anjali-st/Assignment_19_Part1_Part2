d0e3e34b27718ba52be17c81c8d0ed2274431e08
Anjali-st <139098544+Anjali-st@users.noreply.github.com>
2023-07-31 16:16:49 +0530

    Update ProgramPart2.cs

--- ProgramPart2.cs
+++ ProgramPart2.cs
@@ -6,7 +6,83 @@ using System.Threading.Tasks;
 
 namespace ConApp_Assignment_19_Part1
 {
-    internal class ProgramPart2
+    delegate T Operation<T>(T a, T b);
+    internal class Part2
     {
+        public static int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public static int Diff(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+        public static int Mul(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+        public static int Div(int n1, int n2)
+        {
+            return n1/n2;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("This is Generic Delegate\n");
+                Operation<int> add = Add;
+                Operation<int> diff = Diff;
+                Operation<int> mul = Mul;
+                Operation<int> div = Div;
+                Console.WriteLine("Enter Number One");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Number Two");
+                int n2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("\nSelect the Operation to Perform\n1. Addition\n2. Substraction\n3. Multiplication\n4. Division\n");
+                Console.WriteLine("Which function You Want to perform?");
+            again:
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        {
+                            int result = Add(n1, n2);
+                            Console.WriteLine($"\nThe Addition of {n1} and {n2} is {result}");
+                            break;
+                        }
+                    case 2:
+                        {
+                            int result = Diff(n1, n2);
+                            Console.WriteLine($"\nThe Substraction of {n1} and {n2} is {result}");
+                            break;
+                        }
+                    case 3:
+                        {
+                            int result = Mul(n1, n2);
+                            Console.WriteLine($"\nThe Multiplication of {n1} and {n2} is {result}");
+                            break;
+                        }
+                    case 4:
+                        {
+                            int result = Div(n1, n2);
+                            Console.WriteLine($"\nThe Division of {n1} and {n2} is {result}");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("\nSorry....!!!!\nPlease Select the Right Option");
+                            goto again;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
     }
 }
+
